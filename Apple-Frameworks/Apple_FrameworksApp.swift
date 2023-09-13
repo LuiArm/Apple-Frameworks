33e52ca3d1e56ffc9b8fa81c64c5c21f3b951362
LuiArm <Luisarmendariz24@icloud.com>
2023-09-12 22:10:00 -0600

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by luis armendariz on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
