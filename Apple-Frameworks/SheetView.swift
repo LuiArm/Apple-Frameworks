bd63fbe94bce444a160508ee4002ef650bd395eb
LuiArm <Luisarmendariz24@icloud.com>
2023-09-16 23:27:07 -0600

    Add sheet view

---
+++ Apple-Frameworks/SheetView.swift
@@ -0,0 +1,20 @@
+//
+//  SheetView.swift
+//  Apple-Frameworks
+//
+//  Created by luis armendariz on 9/16/23.
+//
+
+import SwiftUI
+
+struct SheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SheetView_Previews: PreviewProvider {
+    static var previews: some View {
+        SheetView()
+    }
+}
